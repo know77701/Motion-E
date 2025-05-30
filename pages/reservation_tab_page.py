4e11e69db59f7baa356f215784b0558b3daa9b20
know77701 <know77701@gmail.com>
2025-05-30 14:17:01 +0900

    메모리 공유해서 처리하도록 수정

--- pages/reservation_tab_page.py
+++ pages/reservation_tab_page.py
@@ -5,48 +5,70 @@ class ReservationTabPage:
     def __init__(self, app_manager,user_chart_page):
         self.app_manager = app_manager
         self.user_chart_page = user_chart_page
+        self.rsrv_panel_arr = []
+        self.rsrv_edit_arr = []
+        self.rsrv_button_arr = []
+        self.rsrv_comboboxe_arr = []
+        self.rsrv_element_list_arr = []
+        self.setting()
+    
+    def setting(self):
+        self.get_reservation_tab_element_list()
+        self.get_rsrv_panel_items()
+        self.get_rsrv_buttons()
+        self.get_rsrv_edits()
+        self.get_rsrv_comboboxes()
+        print(self.rsrv_panel_arr)
+        print(self.rsrv_edit_arr)
+        print(self.rsrv_button_arr)
     
     def get_reservation_tab_element_list(self):
         """예약 탭 최상위 객체 반환"""
         rsrv_tab = self.user_chart_page.get_chart_field("tab")
         if rsrv_tab:
-            return ElementFinder.recursive_children(rsrv_tab, 1,5)
+            self.rsrv_element_list_arr = ElementFinder.recursive_children(rsrv_tab, 1,5)
     
     def get_rsrv_panel_items(self):
         """예약탭 Pane 하위 항목 리스트 반환"""
-        list_items = self.get_reservation_tab_element_list()
         pane_list = []
-        for items in list_items:
+        
+        for items in self.rsrv_element_list_arr:
             pane_items = ElementFinder.find_pane_by_auto_id(items.children(), "radPanel1")
+            
             if pane_items:
                 pane_list.append(pane_items)
-        return pane_list
+
+        self.rsrv_panel_arr = pane_list
     
     def get_rsrv_buttons(self):
         """예약탭 버튼 리스트 반환"""
         btn_arr = []
-        for pane in self.get_rsrv_panel_items():
+        
+        for pane in self.rsrv_panel_arr:
             btn_arr.append(ElementFinder.find_buttons(pane.children()))
-        return btn_arr
+            
+        self.rsrv_button_arr = btn_arr
 
     def get_rsrv_edits(self):
         """예약탭 Edit 리스트 반환"""
         edit_arr = []
-        for pane in self.get_rsrv_panel_items():
+        
+        for pane in self.rsrv_panel_arr:
             edit_arr.append(ElementFinder.find_edits(pane.children()))
-        return edit_arr
+
+        self.rsrv_edit_arr = edit_arr
 
     def get_rsrv_comboboxes(self):
         """예약탭 콤보박스 리스트 반환"""
         combo_arr = []
-        for pane in self.get_rsrv_panel_items():
+        for pane in self.rsrv_panel_arr:
             combo_arr.append(ElementFinder.find_combobox(pane.children()))
-        return combo_arr
+        self.rsrv_comboboxe_arr = combo_arr
     
     def get_rsrv_list_table(self, auto_id):
         """예약탭 테이블 리스트 반환"""
         table_arr = []
-        list_items = self.get_reservation_tab_element_list()
+        list_items = self.rsrv_element_list_arr
         for items in list_items:
             table_items = ElementFinder.find_tables(items.children())
             if table_items:
@@ -55,35 +77,41 @@ class ReservationTabPage:
         return ElementFinder.find_list_items_by_auto_id(table_arr, auto_id)
     
     def get_forward_memo_edit(self):
-        return self.get_rsrv_edits()[0]
+        test = self.get_rsrv_edits()[0][0]
+        print(test)
+        return test
     
     def get_rsrv_memo_edit(self):
-        return self.get_rsrv_edits()[1]
+        test = self.get_rsrv_edits()[0][1]
+        print(test)
+        return test
     
     def get_rsrv_day_edit(self):
-        return self.get_rsrv_edits()[2]
+        return ElementFinder.find_edit_by_automation_id(self.rsrv_edit_arr, "txtRsrvDd")
 
     def get_rsrv_time_edit(self):
-        return self.get_rsrv_edits()[3]
+        return ElementFinder.find_edit_by_automation_id(self.rsrv_edit_arr, "txtRsrvTm")
     
     def get_rsrv_status_edit(self):
-        return self.get_rsrv_edits()[4]
-
-    def get_forward_memo_edit(self):
-        return self.get_rsrv_edits()[0]
-    
+        return ElementFinder.find_edit_by_automation_id(self.rsrv_edit_arr, "txtRsrvPrgrStat")
+        
     def get_calendar_button(self):
-        return self.get_rsrv_buttons()[0]
+        return self.rsrv_button_arr[0][1]
 
     def get_today_button(self):
-        return self.get_rsrv_buttons()[1]
+        return ElementFinder.find_button_by_name(self.rsrv_button_arr, "오늘")
+
+    def get_remove_content_rsrv_button(self):
+        return ElementFinder.find_button_by_name(self.rsrv_button_arr, "내용 지우기")
 
     def get_change_rsrv_button(self):
-        return self.get_rsrv_buttons()[2]
+        return ElementFinder.find_button_by_name(self.rsrv_button_arr, "예약 변경")
 
     def get_new_rsrv_button(self):
-        return self.get_rsrv_buttons()[3]
-    
+        return ElementFinder.find_button_by_name(self.rsrv_button_arr, "신규 예약")
+
+    def get_cancle_rsrv_button(self):
+        return ElementFinder.find_button_by_name(self.rsrv_button_arr, "예약 취소")
     
     def get_rsrv_timetable(self):
         """예약 설정 타임테이블 반환"""
@@ -100,3 +128,18 @@ class ReservationTabPage:
     def get_reservation_elements(self):
         return
     
+    def write_rsrv_memo(self, content):
+        edit = self.get_rsrv_memo_edit()
+        edit.set_focus()
+        edit.set_text("")
+        edit.set_text(content)
+        
+        print(edit.element_info.name)
+    
+    def write_forward_memo(self, content):
+        edit = self.get_forward_memo_edit()
+        edit.set_focus()
+        edit.set_text("")
+        edit.set_text(content)
+        
+        print(edit.element_info.name)
--- tests/test_reservation_tap.py
+++ tests/test_reservation_tap.py
@@ -7,24 +7,36 @@ class TestReservationTab():
     def setup(self, reservation_tab_page):
         self.reservation_tab_page = reservation_tab_page
     
+    @pytest.mark.skip()
     def test_reservation_tab(self):
         test = self.reservation_tab_page.get_rsrv_timetable()
         for i in test.children():
-            print(i.element_info.name)
-            print(i.element_info.control_type)
-            print(i.element_info.automation_id)
-            print(i.children())
+            for el in i.children():
+                print(el)
+                
         test = self.reservation_tab_page.get_rsrv_list()
         for i in test.children():
-            print(i.element_info.name)
-            print(i.element_info.control_type)
-            print(i.element_info.automation_id)
-            print(i.children())
+            for el in i.children():
+                print(el)
+        
+        
         test = self.reservation_tab_page.get_ticket_list()
         for i in test.children():
-            print(i.element_info.name)
-            print(i.element_info.control_type)
-            print(i.element_info.automation_id)
-            print(i.children())
-        rsrv_edit_list = self.reservation_tab_page.get_rsrv_edits()
-        print(rsrv_edit_list)
+            for el in i.children():
+                print(el)
+        
+    def test_new_reserve(self):
+        self.reservation_tab_page.write_rsrv_memo("신규 예약, 예약메모 작성")
+        self.reservation_tab_page.write_forward_memo("신규 예약, 예약메모 작성")
+        
+        test =  self.reservation_tab_page.get_rsrv_buttons()
+        for el in test:
+            print(el.element_info.automation_id)
+            print(el.element_info.name)
+        return
+    
+    def test_cancel_reserve(self):
+        return
+    
+    def test_change_reserve(self):
+        return
